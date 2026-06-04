12187848be5e25a0d1e2a60d68751fc5f6b3fc4a
vamsid-Mass <vamsid2890@gmail.com>
2026-06-04 10:15:04 +0530

    Update webasg.tf

--- webasg.tf
+++ webasg.tf
@@ -14,7 +14,7 @@ resource "aws_autoscaling_group" "swiggy-web-asg" {
 ###### Create a Launch Template for the EC2 instances ######
 resource "aws_launch_template" "swiggy-web-template" {
   name_prefix   = "swiggy-web-template"
-  image_id      = "ami-0685bcc683dadb6b9"
+  image_id      = "ami-00e801948462f718a"
   instance_type = "t3.small"
 
   network_interfaces {
