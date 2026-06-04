b572c38d4d426ec9e5f99a819de5b7fe6fb13cd1
vamsid-Mass <vamsid2890@gmail.com>
2026-06-04 10:18:26 +0530

    Update appasg.tf

--- appasg.tf
+++ appasg.tf
@@ -20,7 +20,7 @@ resource "aws_autoscaling_group" "swiggy-app-asg" {
 # Create a launch template for the EC2 instances
 resource "aws_launch_template" "swiggy-app-template" {
   name_prefix   = "swiggy-app-template"
-  image_id      = "ami-0685bcc683dadb6b9"
+  image_id      = "ami-00e801948462f718a"
   instance_type = "t3.small"
 
   network_interfaces {
