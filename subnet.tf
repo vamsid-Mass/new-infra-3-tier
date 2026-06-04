9cca355ac95e2d81c0d13b29aca9a8fc12cc5356
vamsid-Mass <vamsid2890@gmail.com>
2026-06-04 12:36:15 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -2,7 +2,7 @@
 resource "aws_subnet" "swiggy-pub-sub-1" {
   vpc_id                  = aws_vpc.swiggy-vpc.id
   cidr_block              = "10.0.0.0/28"
-  availability_zone       = "ap-south-1a"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = "true"
 
   tags = {
@@ -13,7 +13,7 @@ resource "aws_subnet" "swiggy-pub-sub-1" {
 resource "aws_subnet" "swiggy-pub-sub-2" {
   vpc_id                  = aws_vpc.swiggy-vpc.id
   cidr_block              = "10.0.0.16/28"
-  availability_zone       = "ap-south-1b"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = "true"
   tags = {
     Name = "swiggy-pub-sub-2"
@@ -25,7 +25,7 @@ resource "aws_subnet" "swiggy-pub-sub-2" {
 resource "aws_subnet" "swiggy-pvt-sub-1" {
   vpc_id                  = aws_vpc.swiggy-vpc.id
   cidr_block              = "10.0.0.32/28"
-  availability_zone       = "ap-south-1a"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = false
   tags = {
     Name = "swiggy-pvt-sub-1"
@@ -35,7 +35,7 @@ resource "aws_subnet" "swiggy-pvt-sub-1" {
 resource "aws_subnet" "swiggy-pvt-sub-2" {
   vpc_id                  = aws_vpc.swiggy-vpc.id
   cidr_block              = "10.0.0.48/28"
-  availability_zone       = "ap-south-1b"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = false
   tags = {
     Name = "swiggy-pvt-sub-2"
@@ -45,7 +45,7 @@ resource "aws_subnet" "swiggy-pvt-sub-2" {
 resource "aws_subnet" "swiggy-pvt-sub-3" {
   vpc_id                  = aws_vpc.swiggy-vpc.id
   cidr_block              = "10.0.0.64/28"
-  availability_zone       = "ap-south-1a"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = false
   tags = {
     Name = "swiggy-pvt-sub-3"
@@ -55,7 +55,7 @@ resource "aws_subnet" "swiggy-pvt-sub-3" {
 resource "aws_subnet" "swiggy-pvt-sub-4" {
   vpc_id                  = aws_vpc.swiggy-vpc.id
   cidr_block              = "10.0.0.80/28"
-  availability_zone       = "ap-south-1b"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = false
   tags = {
     Name = "swiggy-pvt-sub-4"
